807a80d123de20ee71b308a32e327a32bc10dfdc
Adrian Turika <adrian.turika@gmail.com>
2017-08-29 16:55:21 -0500

    New fork with emphasis on design

--- app.js
+++ app.js
@@ -1,34 +1,40 @@
-document.addEventListener('DOMContentLoaded', function(){
-    let form = document.querySelector('form')
-    
-    form.file.addEventListener('change', function(event){
-        let file = event.target.files[0]
-        form.out.innerHTML = `Editting: "${file.name}".`
-        let reader = new FileReader()
-        reader.onload = function(event){
-            let xml = event.target.result
-            console.log(` File is now stored in the DOM.`)
-            form.out.innerHTML += `<h2>${file.name}</h2>`
-            //XML raw data in DOM
-            //console.log(xml)
-            let parser = new DOMParser()
-            let doc = parser.parseFromString(xml, "application/xml")
-            let templateName = doc.querySelector('description')
-            //This makes HTML input value = XML value
-            form.description.value = templateName.textContent
-            // 12HR:0   24HR:1
-            let militaryTime = doc.querySelector('timestyle')
-            militaryTime.textContent
-            
-
-        }
-
-        reader.readAsText(file)
-
-
-    })
 
 
 
 
-});
+
+
+//document.addEventListener('DOMContentLoaded', function(){
+//    let form = document.querySelector('form')[1]
+//    
+//    form.file.addEventListener('change', function(event){
+//        let file = event.target.files[0]
+//        form.out.innerHTML = `Editting: "${file.name}".`
+//        let reader = new FileReader()
+//        reader.onload = function(event){
+//            let xml = event.target.result
+//            console.log(` File is now stored in the DOM.`)
+//            form.out.innerHTML += `<h2>${file.name}</h2>`
+//            //XML raw data in DOM
+//            //console.log(xml)
+//            let parser = new DOMParser()
+//            let doc = parser.parseFromString(xml, "application/xml")
+//            let templateName = doc.querySelector('description')
+//            //This makes HTML input value = XML value
+//            form.description.value = templateName.textContent
+//            // 12HR:0   24HR:1
+//            let militaryTime = doc.querySelector('timestyle')
+//            militaryTime.textContent
+//            
+//
+//        }
+//
+//        reader.readAsText(file)
+//
+//
+//    })
+//
+//
+//
+//
+//});
