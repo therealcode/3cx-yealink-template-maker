ee4dc2a23234b9a2788bdd613e56c262c1d5c9cf
Adrian Turika <adrian.turika@gmail.com>
2017-08-30 13:59:01 -0500

    Update files with new style and code
    app.js = Instead of loading XML file from file input, it pulls the template.xml AS template.txt (for easier I/O & display)
    index.html = Comment out section to upload template file. Add iframe window to display RAW template XML.
    main.css = fix some styling issues.

--- app.js
+++ app.js
@@ -1,40 +1,29 @@
+//document.addEventListener("load", function(){
+//
+//    let templateCode = document.getElementsByName('templateCode').innerHTML
+//    // rawCode isn't defined when run..throws error. But can be accesses by re-defining it through the console. 
+//        
+//    console.log("Resources finished loading!")
+//})
+//
+//document.addEventListener("click", function(){
+//    // Using onclick to trigger replace and re-print for each option change
+//    let rawCode = templateCode.document.body.innerText
+//    rawCode.replace("<header>","<footer>")
+//
+//})
+document.addEventListener("DOMContentLoaded", function(){
+
+    let templateCode = document.getElementsByName('templateCode').innerHTML
+    // rawCode isn't defined when run..throws error. But can be accesses by re-defining it through the console. 
+        
+    console.log("Resources finished loading!")
 
 
+document.addEventListener("click", function(){
+    // Using onclick to trigger replace and re-print for each option change
+    let rawCode = templateCode.document.body.innerText
+    rawCode.replace("<header>","<footer>")
+})
 
-
-
-
-//document.addEventListener('DOMContentLoaded', function(){
-//    let form = document.querySelector('form')[1]
-//    
-//    form.file.addEventListener('change', function(event){
-//        let file = event.target.files[0]
-//        form.out.innerHTML = `Editting: "${file.name}".`
-//        let reader = new FileReader()
-//        reader.onload = function(event){
-//            let xml = event.target.result
-//            console.log(` File is now stored in the DOM.`)
-//            form.out.innerHTML += `<h2>${file.name}</h2>`
-//            //XML raw data in DOM
-//            //console.log(xml)
-//            let parser = new DOMParser()
-//            let doc = parser.parseFromString(xml, "application/xml")
-//            let templateName = doc.querySelector('description')
-//            //This makes HTML input value = XML value
-//            form.description.value = templateName.textContent
-//            // 12HR:0   24HR:1
-//            let militaryTime = doc.querySelector('timestyle')
-//            militaryTime.textContent
-//            
-//
-//        }
-//
-//        reader.readAsText(file)
-//
-//
-//    })
-//
-//
-//
-//
-//});
+})
